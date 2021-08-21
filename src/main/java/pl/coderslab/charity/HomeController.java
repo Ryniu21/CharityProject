8e069170c8ff9f5ce7a70d889696f671612e3470
Ryniu21 <krzysiek.rutkowski89@gmail.com>
2021-08-22 01:11:48 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String homeAction(){
+        return "index";
+    }
+}
